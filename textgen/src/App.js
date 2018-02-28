80634b1cec6ffc21afb10e34bbdda10cc88121d1
oberaij <ijobera@springvalley.tech>
2018-02-28 15:26:49 +0800

    Source folder

---
+++ textgen/src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './App.css';
+import Output from './Components/Output';
+import Select from './Components/Controls/Select';
+import Text from './Components/Controls/Text';
+import axios from 'axios';
+
+
+
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+  
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    };
+  }
+  
+  componentWillMount(){
+    this.getSampleText();
+  }
+
+  getSampleText(){
+    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+      .then((response) => {
+        this.setState({text: response.data.text}, function(){
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+  showHtml(x){
+    this.setState({html: x}, this.getSampleText);
+  }
+  
+  changeParas(number){
+    this.setState({paras: number}, this.getSampleText);
+  }
+  
+  
+  render() {
+    return (
+      <div className="App container">
+      
+        <h1>ReactJS Generator</h1>
+        <hr/>
+        
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
+        
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
+        </form>
+        <Output value={this.state.text} />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
